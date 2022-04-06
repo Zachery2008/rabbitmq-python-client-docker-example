236e1446824d294dd6feef2ddaad54abcff2c26c
zachery2008 <zachery.zhu@lifebooster.ca>
2022-04-05 23:19:22 -0700

    Delete test.py

--- src/test.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-process = subprocess.Popen(['az', 'account', 'list', '--verbose'], stdout=subprocess.PIPE, stderr=subprocess.PIPE,shell=True)
-out, err = process.communicate()
-print(out)
-print(err)
